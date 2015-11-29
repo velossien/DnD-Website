5bd3c8eb4426ab12e4649158564005991f32050a
Tara <tararochegrimm@gmail.com>
2015-11-29 18:24:46 -0500

    add potion store

---
+++ Store Generator/potionGen.js
@@ -0,0 +1,17 @@
+$(document).ready(randomize());
+
+function randomize(){
+	var potion = potionGen();
+	var randoPotion = $(".randoPotion");
+	randoPotion.text(potion);
+	console.log(potion);
+}
+
+function potionGen() {
+	var bubbly =[];
+	for (i=0; i<5; i++) {
+		bubbly.push(potionsPoisons[Math.floor(Math.random() * potionsPoisons.length)]);
+	}
+	
+	return bubbly;	    
+}
---
+++ Store Generator/storeArray.js
@@ -0,0 +1,3 @@
+var potionsPoisons = ["Healing Potion- 3S/1C", "Energy Potion- 4S/8C", "Energizing Potion- 1G/2S/1C","Death Poison- 9S/2C","Fart Poison- 4S/3C"];
+var scrolls = ["Fire Scroll- 3S/1C", "Ice Scrolls- 4S/8C", "Wind Scroll- 1G/2S/1C","Water Scroll- 9S/2C","Fart Scroll- 4S/3C"];
+var weaponsAmmo = ["Axe- 3S/1C", "Long Bow- 4S/8C", "Great Sword- 1G/2S/1C","20 Bolts- 9S/2C","30 Bullets- 4S/3C"];
