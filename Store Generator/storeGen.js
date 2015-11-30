40a2f5dd231a6e612b31240ddc32186a5fda871c
Tara <tararochegrimm@gmail.com>
2015-11-29 21:00:16 -0500

    fixed that shit

--- Store Generator/storeGen.js
+++ Store Generator/storeGen.js
@@ -11,7 +11,7 @@ function randomizePotion(){
 	var potions = storeGen(potionsPoisons);
 	tableGenerate($('.potionTable'), potions);
 	console.log(potions);
-}
+};
 
 //----------------------------------------------------------------------------------
 //main random function for schrivener (calls random function and makes table)
@@ -19,7 +19,7 @@ function randomizeScroll(){
 	var scrollList = storeGen(scrolls);
 	tableGenerate($('.scrollTable'), scrollList);
 	console.log(scrollList);
-}
+};
 
 //----------------------------------------------------------------------------------
 //main random function for weaponsmith (calls random function and makes table)
@@ -27,7 +27,7 @@ function randomizeWeapon(){
 	var weapons = storeGen(weaponsAmmo);
 	tableGenerate($('.weaponTable'), weapons);
 	console.log(weapons);
-}
+};
 
 //-----------------------------------------------------------------------------------
 //generate store
@@ -37,15 +37,15 @@ function storeGen(store) {
 		randomItems.push(store[Math.floor(Math.random() * store.length)]);
 	}
 	return randomItems;	    
-}
+};
 
 //-----------------------------------------------------------------------------------
 //create table
 function tableGenerate (table, items) {
   table.children('tbody').remove();
-  table.append($('tbody'));
+  table.append('<tbody></tbody>');
   for(var i=0; i<items.length;i++)
   {
    table.append('<tr><td>'+items[i].itemName+'</td><td>'+items[i].price+'</td></tr>');
   }
-}
+};
